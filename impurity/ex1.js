d803690dd9706d25246daa6438a650cbe5de9154
Ananya Sharma <ananyas237@gmail.com>
2020-06-06 19:18:47 -0400

    completed task 1

--- impurity/ex1.js
+++ impurity/ex1.js
@@ -7,29 +7,33 @@ var students = [
 	{ id: 74, name: "Jessica" },
 	{ id: 491, name: "Ally" }
 ];
-
-function sortStudentsByName() {
-	// Don't modify this function
-	students.sort(function byName(s1,s2){
-		if (s1.name < s2.name) return -1;
-		else if (s1.name > s2.name) return 1;
-		else return 0;
-	});
-	return students;
-}
-
-function sortStudentsByID() {
+//Task 1 - redcued the surface area of impurity
+function getStudentsByName(students) {
+    //Slicing is declarative
+    students = students.slice();
+      (function sortStudentsByName() {
+        // Don't modify this function
+            students.sort(function byName(s1,s2){
+                if (s1.name < s2.name) return -1;
+                else if (s1.name > s2.name) return 1;
+                else return 0;
+            });
+            return students;
+        })(students);
+        return students;
+    }
+/* function sortStudentsByID() {
 	// Don't modify this function
 	students.sort(function byID(s1,s2){
 		return s1.id - s2.id;
 	});
 	return students;
-}
+} */
 
 // *************************************
 
 // modify/move this function
-function getStudentsByName() { return students; }
+//function getStudentsByName() { return students; }
 
 // modify/move this function
 function getStudentsByID() { return students; }
